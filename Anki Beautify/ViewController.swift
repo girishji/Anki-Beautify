dc54f6d0c5c169970118cf8e421d72e055e3b72b
Girish Palya <girishji@gmail.com>
2019-08-03 20:46:19 +0200

    Initial Commit

---
+++ Anki Beautify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anki Beautify
+//
+//  Created by gpalya on 8/3/19.
+//  Copyright © 2019 gpalya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
