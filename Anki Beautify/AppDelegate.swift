a94e9fed3a0f1fcf01f56ac51a8d6db3d26a27d7
Girish Palya <girishji@gmail.com>
2019-08-08 12:10:12 +0200

    fixed icon files

--- Anki Beautify/AppDelegate.swift
+++ Anki Beautify/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         // girish
-        //PasteboardFormat.transform()
+        PasteboardFormat.transform()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
